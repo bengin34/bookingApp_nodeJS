65d8571bd2629b9dda38e0085e131fc7eb37c2b1
bengin34 <becaglar3434@gmail.com>
2023-05-11 17:04:18 +0300

    user

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,18 +1,27 @@
-import express from "express"
-import Hotel from "../models/Hotel.js"
-import { createError } from "../utils/error.js"
-import { deleteHotel, getHotel, getHotels, updateHotel, createHotel } from "../controllers/hotel.js"
+import express from "express";
+import Hotel from "../models/Hotel.js";
+// import { createError } from "./error.js"
+import {
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+  createHotel,
+} from "../controllers/hotel.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
-const router = express.Router()
+
+const router = express.Router();
+
 
 //CREATE
-router.post("/", createHotel)
+router.post("/", verifyAdmin, createHotel);
 //UPDATE
-router.put("/:id", updateHotel)
+router.put("/:id",verifyAdmin,  updateHotel);
 //DELETE
-router.delete("/:id", deleteHotel)
+router.delete("/:id",verifyAdmin,  deleteHotel);
 //GET
-router.get("/:id", getHotel)
+router.get("/:id", getHotel);
 //GET ALL
-router.get("/", getHotels)
-export default router
+router.get("/", getHotels);
+export default router;
