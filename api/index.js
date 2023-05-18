a678d52d2c40280b28b7c78df94e07098021e48e
bengin34 <becaglar3434@gmail.com>
2023-05-18 08:19:36 +0300

    react app

--- api/index.js
+++ api/index.js
@@ -1,63 +1,57 @@
-import express from "express"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import authRoute from "./routes/auth.js"
-import hotelsRoute from "./routes/hotels.js"
-import usersRoute from "./routes/users.js"
-import roomsRoute from "./routes/rooms.js"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import usersRoute from "./routes/users.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
 
-
-
-const app = express()
-dotenv.config()
-
+const app = express();
+dotenv.config();
 
 const connect = async () => {
-try {
+  try {
     await mongoose.connect(process.env.MONGO);
-    console.log("Connected to mongoDB.")
+    console.log("Connected to mongoDB.");
   } catch (error) {
-    throw error
+    throw error;
   }
-}
-
+};
 
 mongoose.connection.on("disconnected", () => {
-    console.log("mongoDB  disconnected")
-})
+  console.log("mongoDB  disconnected");
+});
 mongoose.connection.on("connected", () => {
-    console.log("mongoDB  connected")
-})
+  console.log("mongoDB  connected");
+});
 
-app.get("/", (req,res) => {
-    res.send("hello first request")
-})
+app.get("/", (req, res) => {
+  res.send("hello first request");
+});
 
 //middlewares
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
-app.use("/api/auth", authRoute)
-app.use("/api/users", usersRoute)
-app.use("/api/hotels", hotelsRoute)
-app.use("/api/rooms", roomsRoute)
-
-app.use((err,req,res,next) => {
-  const errorStatus = err.status || 500
-  const errorMessage = err.message || "Something went wrong"
- return res.status(errorStatus).json({
-    success:false,
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong";
+  return res.status(errorStatus).json({
+    success: false,
     status: errorStatus,
     message: errorMessage,
     stack: err.stack,
- })
-})
-
+  });
+});
 
 app.listen(8800, () => {
-    connect()
-   console.log("Connected to backend!!!") 
-})
+  connect();
+  console.log("Connected to backend!!!");
+});
