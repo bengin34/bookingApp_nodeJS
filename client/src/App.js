697dc2046abe3c92666c03399135c96028a01f2b
bengin34 <becaglar3434@gmail.com>
2023-05-18 09:53:33 +0300

    navbar header

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotel/:id" element={<Hotel />} />
       </Routes>
     </>
   );
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import Header from "../../components/header/Header"
+import Navbar from "../../components/navbar/Navbar"
+import "./home.css"
 
 const Home = () => {
   return (
     <div>
-     <h2>Home</h2> 
+    <Navbar/>
+     <Header/>
     </div>
   )
 }
