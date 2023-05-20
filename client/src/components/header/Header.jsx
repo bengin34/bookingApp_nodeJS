b4e8beda8336980ccf6cf5a78196cd6d31a0cb8f
bengin34 <becaglar3434@gmail.com>
2023-05-20 10:17:28 +0300

    header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -10,18 +10,25 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import { DateRange } from "react-date-range";
-import 'react-date-range/dist/styles.css'; // main css file
-import 'react-date-range/dist/theme/default.css'; // theme css file
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { format } from "date-fns";
 
 const Header = () => {
-
-    const [date, setDate] = useState([
-        {
-          startDate: new Date(),
-          endDate: null,
-          key: 'selection'
-        }
-      ]);
+  const [openDate, setOpenDate] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
   return (
     <div className="header">
       <div className="headerContainer">
@@ -64,17 +71,52 @@ const Header = () => {
           </div>
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
-            <span className="headerSearchText" >date to date</span>
-            <DateRange
-  editableDateInputs={true}
-  onChange={item => setDate([item.selection])}
-  moveRangeOnFirstSelection={false}
-  ranges={date}
-/>
+            <span
+              onClick={() => setOpenDate(!openDate)}
+              className="headerSearchText"
+            >{`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(
+              date[0].endDate,
+              "dd/MM/yyyy"
+            )}`}</span>
+            {openDate && (
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className="date-range"
+              />
+            )}
           </div>
           <div className="headerSearchItem">
             <FontAwesomeIcon icon={faPerson} className="headerIcon" />
-            <span className="headerSearchText" >2 adults 2 children 1 room</span>
+            <span className="headerSearchText">{`${options.adult} adult . ${options.children} . ${options.room} room `}</span>
+            <div className="options">
+              <div className="optionItem">
+                <span className="optionText">Adult</span>
+                <div className="optionCounter">
+                  <button className="optionCounterButton">+</button>
+                  <span className="optionCounterNumber">1</span>
+                  <button className="optionCounterButton">-</button>
+                </div>
+              </div>
+              <div className="optionItem">
+                <span className="optionText">Children</span>
+                <div className="optionCounter">
+                  <button className="optionCounterButton">+</button>
+                  <span className="optionCounterNumber">1</span>
+                  <button className="optionCounterButton">-</button>
+                </div>
+              </div>
+              <div className="optionItem">
+                <span className="optionText">Room</span>
+                <div className="optionCounter">
+                  <button className="optionCounterButton">+</button>
+                  <span className="optionCounterNumber">1</span>
+                  <button className="optionCounterButton">-</button>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="headerSearchItem">
             <button className="headerBtn">Search</button>
