39cb35b55c0d5f4f8081947d1039ff3a310530b6
bengin34 <becaglar3434@gmail.com>
2023-05-22 20:00:43 +0300

    featured

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
+import Featured from "../../components/featured/Featured"
 import "./home.css"
 
 const Home = () => {
@@ -7,8 +8,15 @@ const Home = () => {
     <div>
     <Navbar/>
      <Header/>
+     <div className="homecontainer">
+      <Featured />
+      <Featured />
+     </div>
     </div>
   )
 }
 
 export default Home
+
+
+// 44.12
